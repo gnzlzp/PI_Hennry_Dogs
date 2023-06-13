d3ece84cfa6471632e6a3ee8f8b3bf6dd5f0e642
gnzlzp <gnzlzp86@gmail.com>
2023-06-13 11:32:27 -0500

    deploy api

--- api/index.js
+++ api/index.js
@@ -17,12 +17,15 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require("dotenv").config();
+const { DB_PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(DB_PORT, () => {
+    console.log(`listening at ${DB_PORT}`); // eslint-disable-line no-console
   });
 });
